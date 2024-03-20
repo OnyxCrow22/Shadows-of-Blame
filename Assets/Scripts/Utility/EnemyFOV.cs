2aab26447e1f6b1031e457e81dee472ce5acb651
Luke Hirst <521480@chrome.escg.ac.uk>
2024-03-20 12:20:49 +0000

    Updated the FOV system

--- Assets/Scripts/Utility/EnemyFOV.cs
+++ Assets/Scripts/Utility/EnemyFOV.cs
@@ -7,7 +7,7 @@ public class EnemyFOV : MonoBehaviour
     public AlertStages stages;
     [Range(0, 100)] public float alertLevel;
     public float timeSpentinFOV, fovTimer;
-    public float alertLevelSpeed;
+    public bool canSeePlayer = false;
 
     public enum AlertStages
     {
@@ -19,7 +19,6 @@ public class EnemyFOV : MonoBehaviour
     private void Update()
     {
         alertLevel = Mathf.Clamp(alertLevel, 0, 100);
-        alertLevelSpeed = Mathf.Clamp(alertLevelSpeed, 0, 100);
         timeSpentinFOV = Mathf.Clamp(timeSpentinFOV, 0, 5);
         fovTimer = Mathf.Clamp(fovTimer, 0, 5);
     }
@@ -28,15 +27,19 @@ public class EnemyFOV : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            alertLevel += alertLevelSpeed += timeSpentinFOV;
+            alertLevel += timeSpentinFOV;
             stages = AlertStages.NoticedSomething;
             timeSpentinFOV += fovTimer;
+            fovTimer -= Time.deltaTime;
+            canSeePlayer = true;
 
-            if (timeSpentinFOV >= 5 || alertLevel == 100)
+            if (timeSpentinFOV >= 5 || fovTimer <= 0 || alertLevel == 100)
             {
                 alertLevel = 100;
                 stages = AlertStages.Alerted;
                 timeSpentinFOV = 5;
+                fovTimer = 0;
+                canSeePlayer = true;
             }
         }
     }
@@ -45,15 +48,17 @@ public class EnemyFOV : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            alertLevel -= alertLevelSpeed -= timeSpentinFOV;
-            timeSpentinFOV -= fovTimer;
+            alertLevel -= timeSpentinFOV;
+            fovTimer += Time.deltaTime;
+            canSeePlayer = false;
 
-            if (timeSpentinFOV <= 0 || alertLevel == 0)
+            if (timeSpentinFOV <= 0 || fovTimer >= 5 || alertLevel == 0)
             {
                 alertLevel = 0;
                 timeSpentinFOV = 0;
-                alertLevelSpeed = 0;
+                fovTimer = 5;
                 stages = AlertStages.Patrolling;
+                canSeePlayer = false;
             }
         }
     }
