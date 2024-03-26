98d73458d9bbba4f37bd6cd2271f121771954d0c
Luke Hirst <521480@chrome.escg.ac.uk>
2024-03-26 17:01:48 +0000

    Door mechanic added.

--- Assets/Scripts/Utility/DoorSight.cs
+++ Assets/Scripts/Utility/DoorSight.cs
@@ -1,51 +1,88 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorSight : MonoBehaviour
 {
-    private GameObject door;
+    public bool isOpen = false;
     public GameObject interactKey;
     public PlayerMovementSM playsm;
-    private DoorMechanic doorHandle;
+    Animator doorAnim;
 
-    public void OpenDoor()
+    private void Update()
     {
-        RaycastHit doorHit;
-        Ray doorRay = new Ray(playsm.FOV.transform.position, Vector3.forward);
-        Debug.DrawRay(playsm.FOV.transform.position, Vector3.forward);
-        float RayLength = 5;
-        if (Physics.Raycast(doorRay, out doorHit, RayLength))
-        {
-            if (doorHit.collider.CompareTag("Door"))
-            {
-                interactKey.SetActive(true);
-                door = doorHit.collider.gameObject;
-                if (Input.GetKeyDown(KeyCode.E) && !doorHandle.isOpen)
-                {
-                    StartCoroutine(doorHandle.OpeningDoor());
-                }
-
-            }
-        }
+        OpenDoor();
+        CloseDoor();
     }
 
-   public void CloseDoor()
+    void OpenDoor()
     {
+        Ray doorRay = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(transform.position, transform.forward, Color.blue);
         RaycastHit doorHit;
-        Ray doorRay = new Ray(playsm.FOV.transform.position, Vector3.forward);
-        float RayLength = 5;
+        float RayLength = 2;
         if (Physics.Raycast(doorRay, out doorHit, RayLength))
         {
-            if (doorHit.collider.CompareTag("Door"))
+            if (doorHit.collider.gameObject.tag == "Door")
             {
+                GameObject door = doorHit.collider.transform.root.gameObject;
+                doorAnim = door.GetComponent<Animator>();
                 interactKey.SetActive(true);
-                door = doorHit.collider.gameObject;
-                if (Input.GetKeyDown(KeyCode.E) && doorHandle.isOpen)
+                if (Input.GetKeyDown(KeyCode.E))
                 {
-                    StartCoroutine(doorHandle.ClosingDoor());
+                    StartCoroutine(OpeningDoor());
+                }
+
+            }
+        }
+        /*
+        if (doorHit.collider.gameObject.tag != "Door")
+        {
+            interactKey.SetActive(false);
+        }
+        */
+    }
+
+    void CloseDoor()
+    {
+        Ray doorRay = new Ray(transform.position, transform.forward);
+        RaycastHit doorHit;
+        float RayLength = 2;
+        if (Physics.Raycast(doorRay, out doorHit, RayLength))
+        {
+            if (doorHit.collider.gameObject.tag == "Door")
+            {
+                GameObject door = doorHit.collider.transform.root.gameObject;
+                doorAnim = door.GetComponent<Animator>();
+                interactKey.SetActive(true);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    StartCoroutine(ClosingDoor());
                 }
             }
         }
+        /*
+        if (doorHit.collider.gameObject.tag != "Door")
+        {
+            interactKey.SetActive(false);
+        }
+        */
+    }
+
+    public IEnumerator OpeningDoor()
+    {
+        doorAnim.SetBool("openDoor", true);
+        Debug.Log("DOOR OPENING");
+        isOpen = true;
+        yield return new WaitForSeconds(2);
+        interactKey.SetActive(false);
+    }
+
+    public IEnumerator ClosingDoor()
+    {
+        doorAnim.SetBool("closeDoor", true);
+        Debug.Log("DOOR NOW CLOSING");
+        isOpen = false;
+        yield return new WaitForSeconds(2);
+        interactKey.SetActive(false);
     }
 }
