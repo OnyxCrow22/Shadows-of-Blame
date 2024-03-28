21611debc8a548ed727d8b47f86c83a424174cd4
Luke Hirst <521480@chrome.escg.ac.uk>
2024-03-28 15:58:37 +0000

    St. Mary's Island mostly blocked out

---
+++ Assets/Scripts/Utility/Missions/OnTheRun.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTheRun : MonoBehaviour
+{
+    public Transform Player;
+    [Header("Booleans")]
+    public bool inWestralSquare = false;
+    public bool canAccessWesteria = false;
+    public GameObject[] evidence;
+    public Transform Compound;
+    public GameObject gangLeader;
+    public GameObject[] enemies;
+    // public PoliceLevel police;
+    public GameObject otrTrigger;
+    public GameObject evidenceWall;
+
+    private void Start()
+    {
+        inWestralSquare = false;
+        canAccessWesteria = false;
+    }
+
+    void GoToWestralSquare()
+    {
+
+    }
+
+    void FindEvidenceinWS()
+    {
+
+    }
+
+    void ReadEvidence()
+    {
+
+    }
+
+    void GoToCompound()
+    {
+
+    }
+
+    void KillGangLeader()
+    {
+
+    }
+
+    void KillRemainingEnemies()
+    {
+
+    }
+
+    void TakeEvidenceFromGang()
+    {
+
+    }
+
+    void LosePolice()
+    {
+
+    }
+
+    void GoToKingstonStreet()
+    {
+
+    }
+    
+    void PlaceEvidence()
+    {
+        
+    }
+}
