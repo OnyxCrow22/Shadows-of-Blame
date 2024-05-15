fc095b3faa87e88f0116873cfeb3f70e2c7a4b4a
Luke Hirst <521480@chrome.escg.ac.uk>
2024-05-15 16:47:46 +0100

    STREET LAMPS!!

--- Assets/Scripts/Utility/UI/Tips.cs
+++ Assets/Scripts/Utility/UI/Tips.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class Tips : MonoBehaviour
 {
     public GameObject TipsPanel;
-    public TextMeshProUGUI Tip; 
+    public TextMeshProUGUI Tip;
+    public TimelineClip openingCutscene;
 
 
-
+    public void GameStart()
+    {
+        
+    }
 }
