1531d98599b952dcba5543c03e9804c2fa901ee8
Luke Hirst <521480@chrome.escg.ac.uk>
2024-05-18 15:23:02 +0100

    Oops

--- Assets/Scripts/Utility/Environment/BrightnessControl.cs
+++ Assets/Scripts/Utility/Environment/BrightnessControl.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BrightnessControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider sunBrightnessSlider, moonBrightnessSlider;
+    public TextMeshProUGUI brightnessValue;
+    public Light sunlightIntenstity;
+    public Light moonLightIntenstity;
+
+    private void Awake()
     {
-        
+        sunBrightnessSlider.onValueChanged.AddListener(SetSunIntensity);
+        moonBrightnessSlider.onValueChanged.AddListener(SetMoonIntensity);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        sunBrightnessSlider.value = PlayerPrefs.GetFloat("sunBrightness", 1f);
+        moonBrightnessSlider.value = PlayerPrefs.GetFloat("moonBrightness", 1f);
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat("sunBrightness", sunBrightnessSlider.value);
+        PlayerPrefs.SetFloat("moonBrightness", moonBrightnessSlider.value);
+    }
+
+    public void SetSunIntensity(float value)
+    {
+        sunlightIntenstity.intensity = value;
+    }
+
+    public void SetMoonIntensity(float value)
+    {
+        moonLightIntenstity.intensity = value;
     }
 }
